0d31159b517c838b70cfc12a4283b695a6a9d903
Michele Barre <michele.barre@gmail.com>
2012-08-07 20:33:35 +0200

    First commit

---
+++ hubbub/grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -0,0 +1,16 @@
+package com.grailsinaction
+
+class UserController {
+
+    def scaffold = true
+	
+	def search = {	
+	}
+	
+	def results = {
+		def users = User.findAllByUserIdLike(params.userId)
+		return [users: users, term: params.userId]
+	}
+	
+	
+}
---
+++ hubbub/test/integration/com/grailsinaction/PostIntegrationTests.groovy
@@ -0,0 +1,58 @@
+package com.grailsinaction
+
+import grails.test.*
+
+class PostIntegrationTests extends GroovyTestCase {
+    
+	void testFirstPost(){
+		def user = new User(userId: '1234567B', password: 'secret')
+		user.save()
+
+				def post1 = new Post(content: "First post... w00t!")
+		user.addToPosts(post1)
+		
+		def post2 = new Post(content: "Second post...")
+		user.addToPosts(post2)
+		
+		def post3 = new Post(content: "Third post...")
+		user.addToPosts(post3)
+		
+		assertEquals 3, User.get(user.id).posts.size()	
+	}
+	
+	void testAccessingPosts(){
+		def user = new User(userId: '1234567B', password: 'secret').save()
+		user.addToPosts(new Post(content: "First"))
+		user.addToPosts(new Post(content: "Second"))
+		user.addToPosts(new Post(content: "Third"))
+		
+		def foundUser = User.get(user.id)
+		def postNames = foundUser.posts.collect{it.content}
+		
+		assertEquals (['First','Second', 'Third'], postNames.sort())
+	}
+	
+	void testPostWithTags(){
+		def user = new User(userId: '1234567B', password: 'secret').save()
+		
+		def tagGroovy = new Tag(name:'groovy')
+		def tagGrails = new Tag(name:'grails')
+		user.addToTags(tagGroovy)
+		user.addToTags(tagGrails)
+		assertNotNull tagGroovy
+		
+		def tagNames = user.tags*.name
+		assertEquals(['grails','groovy'], tagNames.sort())
+		
+		def groovyPost = new Post(content:"A groovy post")
+		user.addToPosts(groovyPost)
+		groovyPost.addToTags(tagGroovy)
+		assertEquals 1,groovyPost.tags.size()
+		
+		def bothPost = new Post(content:"A groovy and grails post")
+		user.addToPosts(bothPost)
+		bothPost.addToTags(groovy)
+		bothPost.addToTags(grails)
+		assetEquals 2, bothPost.tags.size()
+	}
+}
